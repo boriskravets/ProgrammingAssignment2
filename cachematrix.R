29bfaaa187614460f021a2177e8c1e771b798e65
Boris Kravets <boris.m.kravets@gmail.com>
2015-09-28 00:17:41 +0300

    hmm file must be saved before commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## My functions do almost the same thing as sample functions in the Assignment do.
-## Difference is using matrix instead of the vector and 'solve()' instead of 'mean()'
+## Difference is using matrix instead of a vector and computing Inverse of this matrix 
+## instead of vector's mean.
 
 ## Write a short comment describing this function (blah)
 
